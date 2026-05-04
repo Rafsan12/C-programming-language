d0341b77e45641467f2ad2c2f0b5e7bb58c40871
Rafsan12 <rafsan.web.2022@gmail.com>
2026-05-04 22:23:11 +0600

    Add numeric limits example

---
+++ Mobule-1/limiations.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a =10;
+    long long int b = 10000000000;
+    float c = 3.123456;
+    double d = 32422.123456;
+    printf("%0.06lf",d);
+    return 0;
+
+}
