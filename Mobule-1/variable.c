b216d8239e8f16f0e0d5c415baa7b3b2d272f17f
Rafsan12 <rafsan.web.2022@gmail.com>
2026-05-04 11:20:37 +0600

    Add variable lesson

---
+++ Mobule-1/variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int raj = 100;
+    printf("The value of raj is: %d", raj);
+
+    float pi = 3.14;
+    printf("\nThe value of pi is: %0.2f", pi);
+
+    char grade = 'A';
+    printf("\nThe grade is: %c", grade);
+
+    
+    return 0;
+}
