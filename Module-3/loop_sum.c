14d5e3acb67f87f586714c3bec67a664b7068090
Rafsan12 <rafsan.web.2022@gmail.com>
2026-05-08 22:13:56 +0600

    Update loop sum to use input

--- Module-3/loop_sum.c
+++ Module-3/loop_sum.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
-{
+{   
     int i;
+    int n;
+    
     int sum = 0;
-    for(i=1;i<=6;i=i+1){
+    scanf("%d",&n);
+    for(i=1;i<=n;i=i+1){
         sum = sum + i;
-            printf("The sum is %d\n",sum);
-
+            
     }
     printf("The sum is %d\n",sum);
     return 0;
