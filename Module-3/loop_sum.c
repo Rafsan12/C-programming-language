725f68558c5731817a5b48111cf643adf5ccf52a
Rafsan12 <rafsan.web.2022@gmail.com>
2026-05-08 22:04:05 +0600

    Add loop sum practice

---
+++ Module-3/loop_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int sum = 0;
+    for(i=1;i<=6;i=i+1){
+        sum = sum + i;
+            printf("The sum is %d\n",sum);
+
+    }
+    printf("The sum is %d\n",sum);
+    return 0;
+}
