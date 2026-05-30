7483c48f364b5af7b6996b11241d6153462be3e0
Rafsan12 <rafsan.web.2022@gmail.com>
2026-05-30 11:34:17 +0600

    replcement probelm solved

---
+++ C_Replacement.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+
+        if(arr[i]<0)
+        {
+            printf("%d ",2);
+     
+        }
+        else if(arr[i]>0)
+        {
+            printf("%d ",1);
+        }
+        else
+        {
+            printf("%d ",0);
+        }
+    }
+  
+   
+    return 0;
+}
