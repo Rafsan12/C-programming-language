047a54148bafedf1ff920d44e78d4dc2a4b20019
Rafsan12 <rafsan.web.2022@gmail.com>
2026-06-07 20:24:41 +0600

    Add Module-10 fgets input example

--- Module-10/input_with_space.c
+++ Module-10/input_with_space.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    
+    char s[100];
+    // scanf("%s", s);
+    // gets(s);
+    fgets(s, 10, stdin);
+    printf("%s\n", s);
     return 0;
 }
