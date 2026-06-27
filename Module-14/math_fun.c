af736f81cf2fc45c669b3086c83790191e99271d
Rafsan12 <rafsan.web.2022@gmail.com>
2026-06-27 09:34:12 +0600

    Update math function pow example

--- Module-14/math_fun.c
+++ Module-14/math_fun.c
@@ -3,13 +3,14 @@
 
 int main()
 {
-    double x;
-    scanf("%lf",&x);
+    double x,y;
+    scanf("%lf %lf",&x,&y);
     // int ans = ceil(x);
     // int ans = floor(x);
     // int ans = round(x);
-    double ans = sqrt(x);
-    printf("%lf",ans);
+    // double ans = sqrt(x);
+int ans = pow(x,y);
+    printf("%d",ans);
 
     return 0;
 }
